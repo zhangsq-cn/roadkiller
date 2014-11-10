2418a8df49e21fac00f6d2a922121fcf7b4dc552
zhangsq <zhangsq@outlook.com>
2014-11-10 13:20:46 +0800

    add startlayer

--- src/PlayScene.js
+++ src/PlayScene.js
@@ -2,10 +2,16 @@ var PlayScene = cc.Scene.extend({
     onEnter : function () {
         this._super();
 
+        //var roadLayer = window.roadLayer = new RoadLayer();
+        //console.log(window);
+        //roadLayer = new RoadLayer();
         this.addChild(new RoadLayer(), 0, 0);
+        //this.addChild(roadLayer);
 
-        //this.addChild(new CarLayer(), 0, 1);
+        this.addChild(new CarLayer(), 0, 1);
 
-        this.addChild(new ControlLayer(), 0, 2);
+        //this.addChild(new ControlLayer(), 0, 2);
+
+        this.addChild(new StartLayer(), 0, 2);
     }
 });
